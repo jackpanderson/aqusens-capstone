ac36a1a424c843117097925254936e63f364c2e1
Deeba Khosravi <deeba.khosravi@gmail.com>
2025-02-06 15:58:45 -0800

    added error checking for NOAA url query

--- testingNOAAwaterLevelAPI.py
+++ testingNOAAwaterLevelAPI.py
@@ -2,16 +2,20 @@ import requests
 
 def queryForWaterLevel():
     url = "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?date=latest&station=9412110&product=water_level&datum=STND&time_zone=lst&units=metric&format=json"
-    # Queries for the current water level from Port San Luis pier, updates every pretty frequently.
+    # Queries for the current water level from Port San Luis pier, updates every 6 minutes.
 
-    response = requests.get(url)
+    # Tries to get NOAA data from url
+    # If connectivity issues or data not found, fallback to SD card information (return -1)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
 
-    if response.status_code == 200:
-        data = response.json()
-
-        water_level = data['data'][0]['v']
-        return water_level
-    else:
+            water_level = data['data'][0]['v']
+            return water_level
+        else:
+            return -1
+    except requests.exceptions.RequestException as e:
         return -1
 
 
