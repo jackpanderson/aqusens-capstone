0ae688647646bd2796c42d4b7e5a228bc76b3811
jjrzov <jjrzov@gmail.com>
2025-02-18 18:59:09 -0800

    Updated sd card file

--- Aqusens-ASRS/SD.ino
+++ Aqusens-ASRS/SD.ino
@@ -1,74 +1,173 @@
-/**
- * @brief Finds the closest tide prediction from the SD
- * 
- * @param nextDropTime String with next drop time in correct format
- * @return double Tide prediction of whatever time is closest
- */
-double findClosestDate(String nextDropTime) {
-  String closestDate = "";
-  char currentChar, previousChar;
-  String year, month, day, time, pred, compDate, line, prevPred, prevCompDate;
 
-  if (!SD.begin(SD_CS)) {
-    Serial.println("Initialization failed!");
+/*---------------------------------------------------------
+* Function: findClosestDate()
+* Purpose: Finds the closest tide prediction from the SD
+* Params: None
+* Returns: interpolated Tide prediction (cm) of whatever times its closest to
+---------------------------------------------------------*/
+float getTideData(){
+  char firstChar;
+  String line;
+  int curr_year, curr_month, curr_day, curr_hour;
+  int itr_year, itr_month, itr_day, itr_hour;
+  float prev_itr_pred, itr_pred, prev_itr_hour;
+
+  prev_itr_pred = -1;
+  prev_itr_hour = -1;
+
+  if (!SD.begin(SD_CS)){
+    Serial.println("Failed to Initialize SD card");
     return NULL;
   }
 
+//  File root = SD.open("/");
+//  listFiles(root, 0);
+  
   File file = SD.open(TIDE_FILE);
-  if (!file) {
-    Serial.println("Failed to open file!");
+  if (!file){
+    Serial.println("Failed to Open Tide File");
     return NULL;
   }
 
-  /* Skip over NOAA file header */
-  while (file.available()) {
-    currentChar = file.read();
-    if (previousChar == '\t' && currentChar == '\t') {  // Finds two tabs (between Date and Day)
-      break;
+  /* Skip over File Header */
+  while (file.available()){
+    line = file.readStringUntil('\n');
+    firstChar = line[0];
+    if (isDigit(firstChar)){
+      break;  /* At the first line of data */
     }
-    previousChar = currentChar;
   }
 
-  file.readStringUntil('\n');   // Skips to end of that line (Date, Day, Time, etc)
+  /* Get Current Time */
+  curr_year = rtc.getYear();  /* In #### format */
+  curr_month = rtc.getMonth();
+  curr_day = rtc.getDay();
+  curr_hour = rtc.getHours();
+  
+  /* Start reading tide data */
+  while (file.available()){
+    // line = file.readStringUntil('\n');  /* Get line of data */
+    
+    Serial.println(line);
+    
+    if (line.length() > 0){
+      line.trim();  /* Removes whitespaces from end and beginning */
 
-  /* Find closest tide time */
-  while (file.available()) {
-    line = file.readStringUntil('\n');  // Grab line off SD
-    line.trim();
-    if (line.length() > 0) {
+      itr_year = line.substring(2,4).toInt();
+      itr_month = line.substring(5,7).toInt();
+      itr_day = line.substring(8,10).toInt();
+      itr_hour = line.substring(15,17).toInt(); 
+      itr_pred = line.substring(21, line.indexOf('\t', 21)).toInt();  // Needs to be updated for military times
 
-      year = line.substring(0,4);     // Pulling all date and time info
-      month = line.substring(5,7);
-      day = line.substring(8,10);
-      time = line.substring(15, 20);
+      Serial.print("Current year: ");
+      Serial.println(curr_year);
+      Serial.print("Data year: ");
+      Serial.println(itr_year);
+      
+      Serial.print("Current month: ");
+      Serial.println(curr_month);
+      Serial.print("Data month: ");
+      Serial.println(itr_month);
 
-      pred = line.substring(21, line.indexOf('\t', 21));  // Grabs tide prediction
+      Serial.print("Current day: ");
+      Serial.println(curr_day);
+      Serial.print("Data day: ");
+      Serial.println(itr_day);
 
-      compDate = String(year + "-" + month + "-" + day + " " + time + ":00"); // Formats date from SD
-
-      /* Checks dates until one is after the next drop time */
-      if (strcmp(nextDropTime.substring(2).c_str(), compDate.substring(2).c_str()) <= 0) {
-
-        /* See if the time before or after the next drop time is closer, returns the closer tide prediction */
-        if (abs(makeTime(parseTime(nextDropTime.c_str())) - makeTime(parseTime(prevCompDate.c_str()))) 
-              > abs(makeTime(parseTime(nextDropTime.c_str())) - makeTime(parseTime(compDate.c_str())))) {
-          file.close();
-          return prevPred.toDouble();   // Returns previous prediction
-        } else {
-          file.close();
-          return pred.toDouble();       // Returns next prediction
-        }
-
+      Serial.print("Current hour: ");
+      Serial.println(curr_hour);
+      Serial.print("Data hour: ");
+      Serial.println(itr_hour);
+    
+      if (curr_year != itr_year){
+        Serial.println("Need to update SD Card\n");
+        file.close();
+        return -1;  /* Need to Update Tide data file */
       }
 
-      /* Saves in case next date is after the drop time to compare */
-      prevPred = pred;
-      prevCompDate = compDate; 
+      if (curr_month == itr_month){
+        if (curr_day == itr_day){
+          if (curr_hour < itr_hour){
+            break;  /* Closest tide data found */
+          }
+        }
+      }
+
+      prev_itr_pred = itr_pred;
+      prev_itr_hour = itr_hour;
+      line = file.readStringUntil('\n');
+    } else {
+      file.close();
+      return -1; /* Reached end of file */
     }
   }
 
-  /* Only reaches this section if no dates are after next drop time (something is wrong) */
+  Serial.print("Prev hour: ");
+  Serial.println(prev_itr_hour);
+
+  /* Interpolate to get an estimate of current tide level */
+  float scaled_curr_hour, hour_diff;
+  float proportion, pred_diff, interpolated_pred;
+  
+  if (prev_itr_pred == -1 and prev_itr_hour == -1){
+    file.close();
+    return itr_pred;  /* First in list is closest match so just return it since there is no prev */
+  }
+
+  if (prev_itr_hour > itr_hour){
+    /* Need to scale time as interpolation is inbetween two days */
+    scaled_curr_hour = (curr_hour + 24) - prev_itr_hour; 
+    hour_diff = (itr_hour + 24) - prev_itr_hour;
+  } else {
+    scaled_curr_hour = curr_hour - prev_itr_hour; 
+    hour_diff = itr_hour - prev_itr_hour;
+  }
+
+  Serial.print("Hour diff: ");
+  Serial.println(hour_diff);
+  Serial.print("Scaled Curr Hour: ");
+  Serial.println(scaled_curr_hour);
+
+  Serial.print("Itr pred: ");
+  Serial.println(itr_pred);
+  Serial.print("Prev Itr pred: ");
+  Serial.println(prev_itr_pred);
+
+  proportion = scaled_curr_hour / hour_diff;
+  pred_diff = itr_pred - prev_itr_pred;
+  interpolated_pred = pred_diff * proportion;
+  
+  Serial.print("Prop: ");
+  Serial.println(proportion, 5);
+  
+  Serial.print("Pred Diff: ");
+  Serial.println(pred_diff);
+
+  Serial.print("Interpolated Pred: ");
+  Serial.println(interpolated_pred);
+  
   file.close();
-  Serial.println("No tide prediction found.");
-  return NULL;
+  return interpolated_pred;
+}
+
+void listFiles(File dir, int numTabs) {
+  while (true) {
+    File entry = dir.openNextFile();
+    if (!entry) {
+      // No more files
+      break;
+    }
+    for (int i = 0; i < numTabs; i++) {
+      Serial.print("\t");
+    }
+    Serial.print(entry.name());
+    if (entry.isDirectory()) {
+      Serial.println("/");
+      listFiles(entry, numTabs + 1); // Recursive call for subdirectories
+    } else {
+      Serial.print("\t");
+      Serial.println(entry.size(), DEC); // Print file size
+    }
+    entry.close();
+  }
 }
